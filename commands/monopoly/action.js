9e01fb3a00f80f4915bc14178526f5f87f4e2256
Jordan Black <jordan.black213@gmail.com>
2021-03-15 21:46:00 -0500

    updated monopoly scores

--- commands/monopoly/action.js
+++ commands/monopoly/action.js
@@ -4,11 +4,12 @@ exports.run = (bot, message, args, func) => {
 
     const embed = new Discord.MessageEmbed()
       .setTitle('Monopoly Leaderboard')
+      .setDescription('Action Cards')
       .setThumbnail('https://vignette.wikia.nocookie.net/monopoly/images/4/43/Sr_monopoly_vota.png/revision/latest?cb=20180808000359')
-      .setColor('#8FBC72')
-      .addField('Jordan','Score: 109', false)
-      .addField('Wolf','Score: 99', false )
-      .addField('David','Score: 100', false )
+      .setColor('#FF0000')
+      .addField('Jordan','Score: 106', false)
+      .addField('Wolf','Score: 103', false )
+      .addField('David','Score: 102', false )
       .addField('Azad','Score: -950', false )
       message.channel.send({embed});
 }
