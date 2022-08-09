25bcdd0ad58298348140176b96df751d573a5b02
Jordan Black <jordan.black213@gmail.com>
2022-08-09 03:42:57 -0500

    error checking, sidebar, oauth

---
+++ dashboard/routes/dashboard-routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const client = require('../../index')
+
+const router = express.Router();
+
+router.get('/dashboard', (req,res) => res.render('dashboard/index'));
+
+router.get('/servers/:id', (req,res) => res.render('dashboard/show', { 
+    guild: client.guilds.cache.get(req.params.id) 
+}));
+
+module.exports = router;
--- dashboard/server.js
+++ dashboard/server.js
@@ -2,20 +2,30 @@ require('dotenv').config();
 
 const path = require('path')
 const express = require('express');
-const { commands }  = require('../handler/index');
-
+const cookies = require('cookies')
 const authRoutes = require('./routes/auth-routes');
 const rootRoutes = require('./routes/root-routes');
+const dashboardRoutes = require('./routes/dashboard-routes');
+const middleware = require('./modules/middleware');
 
 const app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
+app.use(cookies.express('a', 'b', 'c'));
+
 app.use('/static',express.static(path.join(__dirname, '/assets')));
 app.locals.basedir = `${__dirname}/assets`;
 
-app.use('/', rootRoutes, authRoutes);
+app.use('/', 
+    middleware.updateUser,
+    rootRoutes, 
+    authRoutes,
+    middleware.validateUser, middleware.updateGuilds, dashboardRoutes,
+);
+
+app.all('*', (req, res) => res.render('errors/404'));
 
 const port = process.env.PORT || 3000;
 
