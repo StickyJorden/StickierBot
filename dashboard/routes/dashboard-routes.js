2305c1ea97fddd395b7664c20fe68021e75d27c8
Jordan Black <jordan.black213@gmail.com>
2022-08-09 19:28:17 -0500

    general tab

--- dashboard/routes/dashboard-routes.js
+++ dashboard/routes/dashboard-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { guilds } = require('../../index');
 const client = require('../../index');
 const { valiadateGuild } = require('../modules/middleware');
 
@@ -6,6 +7,26 @@ const router = express.Router();
 
 router.get('/dashboard', (req,res) => res.render('dashboard/index'));
 
-router.get('/servers/:id', valiadateGuild, (req,res) => res.render('dashboard/show'));
+router.get('/servers/:id', valiadateGuild, async (req,res) => res.render('dashboard/show', { } ));
+
+router.put('/servers/:id/:module', valiadateGuild, async (req,res) => {
+
+    try {
+        
+        const { id, module } = req.params;
+
+        const saveGuild = await guilds.cache.get(id);
+
+        saveGuild[module] = req.body; 
+
+        await saveGuild.save();
+
+        res.redirect(`/servers/${id}`);
+
+    } catch {
+        res.render(`errors/400`);
+    }
+
+});
 
 module.exports = router;
--- dashboard/server.js
+++ dashboard/server.js
@@ -1,18 +1,25 @@
 require('dotenv').config();
 
+
 const path = require('path')
 const express = require('express');
-const cookies = require('cookies')
+const cookies = require('cookies');
+const middleware = require('./modules/middleware');
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
 const authRoutes = require('./routes/auth-routes');
 const rootRoutes = require('./routes/root-routes');
 const dashboardRoutes = require('./routes/dashboard-routes');
-const middleware = require('./modules/middleware');
+
 
 const app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 app.use(cookies.express('a', 'b', 'c'));
 
 app.use('/static',express.static(path.join(__dirname, '/assets')));
